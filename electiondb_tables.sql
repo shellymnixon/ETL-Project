4d19dbbab91adcae3edd938a800b8b3b07a94733
Shelly Nixon <shellymnixon@gmail.com>
2020-11-27 11:32:50 -0500

    updated tables and ERD

--- electiondb_tables.sql
+++ electiondb_tables.sql
@@ -15,43 +15,30 @@ CREATE TABLE voter_hist (
     voted_county_desc varchar   NOT NULL,
     vtd_label varchar   NOT NULL,
     vtd_description varchar   NOT NULL,
-    CONSTRAINT pk_ncvhis PRIMARY KEY (
+    CONSTRAINT pk_voter_hist PRIMARY KEY (
         ncid
      )
 );
 
 CREATE TABLE voter_demo (
     ncid varchar   NOT NULL,
-    last_name varchar   NOT NULL,
-    first_name varchar   NOT NULL,
-    middle_name varchar   NOT NULL,
-    name_suffix_lbl char   NOT NULL,
-    res_street_address varvarchar   NOT NULL,
     res_city_desc varchar   NOT NULL,
     state_cd varchar   NOT NULL,
     zip_code char   NOT NULL,
-    mail_addr1 varchar   NOT NULL,
-    mail_addr2 varchar   NOT NULL,
-    mail_addr3 varchar   NOT NULL,
-    mail_addr4 varchar   NOT NULL,
-    mail_city varchar   NOT NULL,
-    mail_state varchar   NOT NULL,
-    mail_zipcode char   NOT NULL,
-    full_phone_number varchar   NOT NULL,
     birth_year char   NOT NULL,
     drivers_lic char   NOT NULL,
+    voter_reg_num char   NOT NULL,
+    status_cd char   NOT NULL,
+    voter_status_desc varchar   NOT NULL,
+    reason_cd varchar   NOT NULL,
+    voter_status_reason_desc varchar   NOT NULL,
     race_code char   NOT NULL,
     ethnic_code char   NOT NULL,
     party_cd char   NOT NULL,
     gender_code char   NOT NULL,
     birth_age int   NOT NULL,
     birth_state varchar   NOT NULL,
-    registr_dt char   NOT NULL,
-    status_cd char   NOT NULL,
-     voter_reg_num char   NOT NULL,
-    voter_status_desc varchar   NOT NULL,
-    reason_cd varchar   NOT NULL,
-    voter_status_reason_desc varchar   NOT NULL
+    registr_dt char   NOT NULL
 );
 
 CREATE TABLE voter_dist (
@@ -80,12 +67,29 @@ CREATE TABLE voter_dist (
     vtd_desc varchar   NOT NULL
 );
 
-ALTER TABLE ncvoterdemo ADD CONSTRAINT fk_ncvoterdemo_ncid FOREIGN KEY(ncid)
-REFERENCES ncvhis (ncid);
+CREATE TABLE voter_identifiers (
+    ncid char   NOT NULL,
+    last_name varchar   NOT NULL,
+    first_name varchar   NOT NULL,
+    middle_name varchar   NOT NULL,
+    name_suffix_lbl char   NOT NULL,
+    res_street_address varvarchar   NOT NULL,
+    mail_addr1 varchar   NOT NULL,
+    mail_addr2 varchar   NOT NULL,
+    mail_addr3 varchar   NOT NULL,
+    mail_addr4 varchar   NOT NULL,
+    mail_city varchar   NOT NULL,
+    mail_state varchar   NOT NULL,
+    mail_zipcode char   NOT NULL,
+    full_phone_number varchar   NOT NULL
+);
 
-ALTER TABLE ncvoterstatus ADD CONSTRAINT fk_ncvoterstatus_ncid FOREIGN KEY(ncid)
-REFERENCES ncvhis (ncid);
+ALTER TABLE voter_demo ADD CONSTRAINT fk_voter_demo_ncid FOREIGN KEY(ncid)
+REFERENCES voter_hist (ncid);
 
-ALTER TABLE ncvoterdist ADD CONSTRAINT fk_ncvoterdist_ncid FOREIGN KEY(ncid)
-REFERENCES ncvhis (ncid);
+ALTER TABLE voter_dist ADD CONSTRAINT fk_voter_dist_ncid FOREIGN KEY(ncid)
+REFERENCES voter_hist (ncid);
+
+ALTER TABLE voter_identifiers ADD CONSTRAINT fk_voter_identifiers_ncid FOREIGN KEY(ncid)
+REFERENCES voter_hist (ncid);
 
